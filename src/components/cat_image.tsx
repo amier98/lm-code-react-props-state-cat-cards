3921f78ac0af06ab3e6a2427f694629bd99e8de2
Amier Ali <amier_1998@hotmail.com>
2023-12-09 02:41:52 +0000

    feat: add image license and url

--- src/components/cat_image.tsx
+++ src/components/cat_image.tsx
@@ -1,12 +1,31 @@
 interface catImage {
   image: string;
   altText: string;
+  licenseType: string;
+  licenseURL: string;
+  atributionName: string;
+  attributionURL: string;
 }
 
-const CatImage: React.FC<catImage> = ({ image, altText }) => {
+const CatImage: React.FC<catImage> = ({
+  image,
+  altText,
+  licenseType,
+  licenseURL,
+  atributionName,
+  attributionURL,
+}) => {
   return (
     <>
       <img className="card__image" src={image} alt={altText} />
+      <p className="card_text_small">
+        Image licensed under <a href={licenseURL}>{licenseType}</a>
+        {atributionName && (
+          <>
+            &nbsp;by <a href={attributionURL}>{atributionName}</a>
+          </>
+        )}
+      </p>
     </>
   );
 };
